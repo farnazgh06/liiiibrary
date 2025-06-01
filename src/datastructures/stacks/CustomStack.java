e4e73cd3c40d6d15e8408a2b299bdd3a33f5f2f7
farnaz <whoisfarnaz@gmail.com>
2025-06-02 00:46:54 +0330

    add stacks-CustomStack

---
+++ src/datastructures/stacks/CustomStack.java
@@ -0,0 +1,67 @@
+package datastructures.stacks;
+
+import datastructures.interfaces.Queue;
+import datastructures.lists.CustomLinkedList;
+
+import java.util.NoSuchElementException;
+
+public class CustomStack<T> implements Queue<T> {
+    private CustomLinkedList<T> list;
+
+    public CustomStack() {
+        list = new CustomLinkedList<>();
+    }
+
+    @Override
+    public boolean add(Object t) {
+        if (!offer(t)) throw new IllegalStateException("Stack is full");
+        return true;
+    }
+
+    @Override
+    public boolean offer(Object t) {
+        if (t == null){
+            return false;
+        }
+        list.add(t);
+        return true;
+    }
+    @Override
+    public T remove() {
+        if (list == null) throw new NoSuchElementException();
+        return list.removeFirst();
+    }
+
+    @Override
+    public T poll() {
+        if(list == null){
+            return null;
+        }
+        return list.removeFirst();
+    }
+
+    @Override
+    public T element() {
+        if (list == null) throw new NoSuchElementException();
+        return list.getFirst();
+    }
+
+    @Override
+    public T peek() {
+        if(list == null){
+            return null;
+        }
+        return list.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+}
